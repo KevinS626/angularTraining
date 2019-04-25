1921253f1d98441766436e6c0d1ad0d7ca9702b1
Kevin <kschriver@ussisolutions.com>
2019-04-25 09:43:01 -0400

    not working all that well

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -3,25 +3,26 @@ import { ProjectsModule } from './projects/projects.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
 import { NxModule } from '@nrwl/nx';
 import { MaterialModule } from '@workshop/material';
 
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UiLoginModule } from '@workshop/ui-login';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    HomeModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HomeModule,
     ProjectsModule,
-    CustomersModule
+    UiLoginModule,
+    CustomersModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
